758bd7aa01ff43d937035808780e4b82da398afe
jjxmonster <kuba.tarabasz@wp.pl>
2021-10-28 20:35:01 +0200

    add delete task from done tasks list

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -4,7 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 const config = {
    apiKey: process.env.REACT_APP_APIKEY,
    authDomain: process.env.REACT_APP_AUTHDOMAIN,
-   projectId: process.env.REACT_APP_PID,
+   projectId: 'todo-67d1a',
    storageBucket: process.env.REACT_APP_SB,
    messagingSenderId: process.env.REACT_APP_SID,
    appId: process.env.REACT_APP_APPID,
